ca06ad37e519368dbab5094adebe3325cb9e5b95
Fernando García <fernandgarcia97@hotmail.com>
2021-10-14 22:00:18 -0500

    Puerto agregado

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ db.sequelize
   .sync({ force: true })
   .then(() => {
     console.log("Drop and re-sync db.");
-    const PORT = config.APP_PORT || 3000;
+    const PORT = config.APP_PORT || process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Listening on ${PORT}`));
   })
   .catch((error) => {
